68beccfe7a079382a3b399e6de0b664c841d5f3a
Konstantin_Maksimov <godno.mm@gmail.com>
2022-10-27 11:00:33 +0500

    hello white

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -4,6 +4,10 @@ namespace HelloApp
 {
     class Program
     {
+        public static void GreetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
         static void Main()
         {
             Console.WriteLine("Hello, world!");
