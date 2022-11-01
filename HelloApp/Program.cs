d905765d40a19cf0bc21cae8754c5be6b58ca7c9
black <black@example.com>
2022-11-01 13:54:03 +0500

    Hello everyone

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -10,7 +10,7 @@ namespace HelloApp
         }
         static void Main()
         {
-            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Hello everyone!");
         }
     }
 }
